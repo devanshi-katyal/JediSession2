042e555273feb343ca6e7a2b063e12d199088b4c
Devanshi Katyal <devanshi.katyal@BLRETV-C02WP1EV.local>
2022-06-07 08:41:34 +0530

    initial commit

---
+++ src/com/flipkart/demo/Demoapp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author devanshi.katyal
+ *
+ */
+public class Demoapp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("this is my first project");
+	}
+
+}
